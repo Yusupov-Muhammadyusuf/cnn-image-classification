c3369d8f5e3b076a5fd237b3de12f737cbac27a5
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2026-03-18 06:37:56 +0500

    feat: add initial dataset

---
+++ dataset.py
@@ -0,0 +1,38 @@
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+
+def get_dataloaders(batch_size=32):
+
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    train_dataset = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform
+    )
+
+    test_dataset = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transform
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    return train_loader, test_loader
